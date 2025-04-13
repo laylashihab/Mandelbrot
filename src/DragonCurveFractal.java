f0e8d4dc3ccae9c9203172b27988185c5ecf18a4
laylashihab <lshihab@purdue.edu>
2025-04-12 20:08:42 -0400

    can change generation while frame is running in dragon curve fractal

--- src/DragonCurveFractal.java
+++ src/DragonCurveFractal.java
@@ -1,14 +1,37 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.geom.Path2D;
 
 public class DragonCurveFractal extends JComponent {
-    Graphics2D g2;
-    Path2D.Double initializer;
+    static Graphics2D g2;
+    static Path2D.Double initializer;
     private static JFrame frame;
 
     static int sideLength;
-    static int maxGen = -1;
+    static int maxGen = 1;
+
+    static Button nextGenButton;
+    static Button lastGenButton;
+
+    static ActionListener actionListener = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == (nextGenButton)) {
+                maxGen++;
+                nextGeneration(1);
+                frame.repaint();
+                frame.revalidate();
+            }
+            if (e.getSource() == (lastGenButton)) {
+                maxGen--;
+                nextGeneration(1);
+                frame.repaint();
+                frame.revalidate();
+            }
+        }
+    };
 
     public DragonCurveFractal() {
     }
@@ -21,13 +44,15 @@ public class DragonCurveFractal extends JComponent {
         frame.setResizable(true);
         frame.setLocationRelativeTo(null); //centers frame on screen
 
-        while (maxGen == -1) {
-            try {
-                maxGen = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter a number of generations:"));
-            } catch (NumberFormatException e) {
-                JOptionPane.showMessageDialog(null, "Please enter a number");
-            }
-        }
+        JPanel content = new JPanel();
+        nextGenButton = new Button("Next Generation");
+        nextGenButton.addActionListener(actionListener);
+        lastGenButton = new Button("Last Generation");
+        lastGenButton.addActionListener(actionListener);
+        content.add(nextGenButton);
+        content.add(lastGenButton);
+        frame.add(content, BorderLayout.NORTH);
+
         sideLength = 256;
 
         frame.add(new DragonCurveFractal());
@@ -41,18 +66,18 @@ public class DragonCurveFractal extends JComponent {
         g2 = (Graphics2D) g;
 
         // sets coordinate origin to center
-        g2.setStroke(new BasicStroke(4));
+        g2.setStroke(new BasicStroke(5));
         g2.setColor(Color.PINK);
 
         int[] xPoints;
         int[] yPoints;
 
         // points to create initializer
-        xPoints = new int[] {0, sideLength/2, sideLength};
-        yPoints = new int[] {0, sideLength/2, 0};
+        xPoints = new int[]{0, sideLength / 2, sideLength};
+        yPoints = new int[]{0, sideLength / 2, 0};
 
         // sets coordinate origin to first point
-        g2.translate(getWidth()/2-sideLength/2, getHeight()/2);
+        g2.translate(getWidth() / 2 - sideLength / 2, getHeight() / 2);
 
         // stores two lines that form a 90 degree angle to manipulated to form curve
         initializer = new Path2D.Double();
@@ -66,16 +91,15 @@ public class DragonCurveFractal extends JComponent {
         }
 
         nextGeneration(1);
-
     }
 
-    public void nextGeneration(int gen){
+    public static void nextGeneration(int gen) {
         // rotate and scale
         g2.rotate(Math.PI / 4);
-        g2.scale(Math.sqrt(2)/2f,Math.sqrt(2)/2f);
+        g2.scale(Math.sqrt(2) / 2f, Math.sqrt(2) / 2f);
 
-        if (gen< maxGen) {
-            nextGeneration(gen+1);
+        if (gen < maxGen) {
+            nextGeneration(gen + 1);
         } else {
             // draw half of next gen
             g2.draw(initializer);
@@ -83,24 +107,25 @@ public class DragonCurveFractal extends JComponent {
 
         // move origin
         g2.rotate(-Math.PI / 4);
-        g2.scale(2/Math.sqrt(2),2/Math.sqrt(2));
+        g2.scale(2 / Math.sqrt(2), 2 / Math.sqrt(2));
         g2.translate(sideLength, 0);
-        g2.scale(Math.sqrt(2)/2f,Math.sqrt(2)/2f);
+        g2.scale(Math.sqrt(2) / 2f, Math.sqrt(2) / 2f);
         g2.rotate(Math.PI / 4);
 
         // rotate and draw second half of next gen
-        g2.rotate(Math.PI/2);
+        g2.rotate(Math.PI / 2);
 
-        if (gen< maxGen) {
-            nextGeneration(gen+1);
+        if (gen < maxGen) {
+            nextGeneration(gen + 1);
         } else {
             g2.draw(initializer);
         }
 
         // move origin back to first point
-        g2.rotate(-3*Math.PI / 4);
-        g2.scale(2/Math.sqrt(2),2/Math.sqrt(2));
+        g2.rotate(-3 * Math.PI / 4);
+        g2.scale(2 / Math.sqrt(2), 2 / Math.sqrt(2));
         g2.translate(-sideLength, 0);
     }
+
 
 }
